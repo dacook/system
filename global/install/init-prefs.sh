934304328799226722b2fbf5959e18baf1466b82
David Cook <dcook@squiz.net>
2017-12-01 11:03:58 +1100

    Fixes

--- global/install/init-prefs.sh
+++ global/install/init-prefs.sh
@@ -1,23 +1,29 @@
 # Initialise Preferences
-# todo: finishe this and test and use it. I don't think pushd likes tilde
 # also load OS-specific and work-specific stuff
+# 
+# prereq: install  Tomorrow colour scheme first to avoid sublime going crazy.
+
+set -e # Exit on error
+set -x # Display each command so user is aware of what's happening
+home=/users/$USER
+# config=$0/../config #~/system/global/config
+config=$home/system/global/config 
+
 
 ## Link version controlled files in the relevant places
-config=$0/../config #~/system/global/config
-
-pushd ~/
+pushd $home/
 	ln -snf "$config/.bash_profile"
 	ln -snf "$config/.bash_aliases"
 popd
 
 ## Sublime
-pushd "~/Library/Application Support/Sublime Text 3/Packages"
+pushd "$home/Library/Application Support/Sublime Text 3/Packages"
     mv User User.bak
 	ln -snf "$config/sublime/User"
 popd
 
 ## DiffMerge
-pushd ~/Library/Preferences/
+pushd $home/Library/Preferences/
 	ln -snf "$config/SourceGear\ DiffMerge\ Preferences"
 popd
 
@@ -30,7 +36,7 @@ git config --global alias.ci commit
 git config --global alias.st status
 # https://gist.github.com/dacook/7eddd0c97b2844cc9157ba2e2f0470bd
 git config --global alias.hist "log --pretty=format:'%C(blue)%ad%C(reset) %C(yellow)%h%C(reset) %C(green)%an%C(reset): %s %C(blue)%d%C(reset)' --graph --date=short"
-git config --global alias.merge-upstream = !git fetch upstream && git merge upstream/master
+git config --global alias.merge-upstream '!git fetch upstream && git merge upstream/master'
 ### DiffMerge (http://twobitlabs.com/2011/08/install-diffmerge-git-mac-os-x/)
 git config --global difftool.prompt false
 git config --global difftool.diffmerge.cmd 'diffmerge "$LOCAL" "$REMOTE"'
